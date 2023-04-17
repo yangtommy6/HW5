2800b93c3ef9d87495638eda13f75ec98a46f065
yangtommy6 <shiningheart0728@gmail.com>
2023-04-17 00:00:27 -0600

    fef

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
 import streamlit as st
 from fastai.vision.all import *
 from fastai.vision.widgets import *
-from PIL import Image
 
-# Load the pre-trained model
+# Load the trained model
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model_path = "model.pkl"
-    learn = load_learner(model_path)
-    return learn
+    model_path = 'model.pkl'
+    model = load_learner(model_path)
+    return model
 
+def classify_image(model, img):
+    return model.predict(img)[0]
+
+# Set up the Streamlit app
 def main():
     st.title("Cat or Dog Classifier")
-    st.write("Upload an image, and the classifier will tell you if it's a cat or a dog.")
+    st.write("Upload an image, and the classifier will tell you if it's a cat or a dog!")
 
-    # Upload image
+    # Load the trained model
+    model = load_model()
+
+    # Allow user to upload an image
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         # Display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-        
-        # Make prediction
-        with st.spinner("Predicting..."):
-            learn = load_model()
-            pred, _, _ = learn.predict(PILImage.create(uploaded_file))
+        img = PIL.Image.open(uploaded_file)
+        st.image(img, caption="Uploaded Image", use_column_width=True)
 
-        # Display the result
-        st.header(f"Prediction: {pred.capitalize()}")
+        # Classify the image using the model
+        label = classify_image(model, img)
+        st.write(f"Prediction: {label.capitalize()}")
 
 if __name__ == "__main__":
     main()
